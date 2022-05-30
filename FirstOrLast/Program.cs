42c28c67be7ba65246e9d575df0bccda0102b678
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-05-30 14:28:21 +0300

    add new projects

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //programm palub kasutajal sisestada ees- ja perekonnanime
+            //programm kontroll andmete pikkust (eraldi nimi ja perekonnanimi)
+            //programm kuvab kumb nendest on pikem, kas ees- või perekonnanimi
+            //if'iga pikkuse teada andmine
+
+
+            Console.WriteLine("Palun, sisesta oma eesnimi:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit.");
+
+
+            Console.WriteLine("Palun, sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Sinu perekonnanimes on {lastName.Length} sümbolit.");
+
+            if (firstName.Length < lastName.Length)
+            {
+                Console.WriteLine("Sinu perekonnanimi on pikem kui eesnimi, kuna sisaldab rohkem sümboleid.");
+            }
+            else if (firstName.Length > lastName.Length)
+            {
+                Console.WriteLine("Sinu eesnimi on pikem kui perekonnanimi, kuna sisaldab rohkem sümboleid.");
+            }
+            else
+            {
+                Console.WriteLine("Mõlemad nimed on sama pikad.");
+            }
+        }
+	}
+}
---
+++ HelloWorldReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorldReverse
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//programm kuvab lauset hello world tagurpidi.
+
+			string lauseHello2 = ("Hello, World!");
+			Console.WriteLine(lauseHello2);
+
+			for (int i = lauseHello2.Length - 1; i >= 0; i--)
+			{
+				Console.WriteLine(lauseHello2[i]);
+
+			}
+
+		}
+	}
+}
